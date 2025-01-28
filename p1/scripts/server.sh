aeb76bea63b5d6a7b30eb7ee48b167cb114724e0
nattensleplubo <nathan.gobert2002@gmail.com>
2025-01-28 09:02:34 +0100

    jou

---
+++ p1/scripts/server.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt update
+sudo apt install curl -y
+echo 'INSTALLING k3s'
+export $(grep -v '^#' /home/vagrant/.env | xargs)
+sudo sh -c "curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC='--node-ip 192.168.56.110 --flannel-iface eth1 --token $TOKEN' sh -"
---
+++ p2/scripts/server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt update
+sudo apt install curl -y
+sudo sh -c 'curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip 192.168.56.110 --flannel-iface eth1 --token mycustomtoken" sh -'
