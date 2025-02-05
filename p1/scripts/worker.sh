818c591708467c8139a4215fa5c4ff629c328028
laboss <laboss@laboss.fr>
2025-02-05 19:49:50 +0100

    i need top

--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
 sudo apt install curl -y
-echo 'INSTALLING K3S'
+echo 'INSTALLING k3s'
 export $(grep -v '^#' /home/vagrant/.env | xargs)
 sudo curl -sfL https://get.k3s.io | sudo K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$TOKEN INSTALL_K3S_EXEC="--node-ip 192.168.56.111 --flannel-iface eth1" sh -
