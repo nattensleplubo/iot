ae55592dfd90fbc6ea25668f6bd28c4a5a2c596c
nattensleplubo <nathan.gobert2002@gmail.com>
2025-02-06 21:04:18 +0100

    hmmmmm

--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -4,10 +4,9 @@ sudo apt install curl -y
 echo 'INSTALLING k3s'
 export $(grep -v '^#' /home/vagrant/.env | xargs)
 sudo sh -c "curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC='--node-ip 192.168.56.110 --flannel-iface eth1 --token $TOKEN' sh -"
-sudo sh -c "sudo kubectl create -f /home/vagrant/app1.yaml"
-sudo sh -c "sudo kubectl apply -f /home/vagrant/app1.yaml"
-sudo sh -c "sudo kubectl create -f /home/vagrant/app2.yaml"
-sudo sh -c "sudo kubectl apply -f /home/vagrant/app2.yaml"
-sudo sh -c "sudo kubectl apply -f /home/vagrant/ingress.yaml"
+sudo kubectl create -f /home/vagrant/app1-deploy.yaml
+sudo kubectl apply -f /home/vagrant/app1-service.yaml
+sudo kubectl create -f /home/vagrant/app2-deploy.yaml
+sudo kubectl apply -f /home/vagrant/app2-service.yaml
+sudo kubectl apply -f /home/vagrant/ingress.yaml
 
-#sudo sh -c "kubectl apply --recursive -f /vagrant/confs/"
