888fb3bd6fa0f238475f5d5dd42c860c0b56f4ad
nattensleplubo <nathan.gobert2002@gmail.com>
2025-02-06 19:12:49 +0100

    app1

--- p2/scripts/server-backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt update
-sudo apt install curl -y
-echo 'INSTALLING k3s'
-export $(grep -v '^#' /home/vagrant/.env | xargs)
-sudo sh -c "curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC='--node-ip 192.168.56.110 --flannel-iface eth1 --token $TOKEN' sh -"
-sudo sh -c "sudo kubectl create configmap app1-html --from-file /home/vagrant/app1.html"
-sudo sh -c "sudo kubectl apply -f /home/vagrant/app1.yaml"
